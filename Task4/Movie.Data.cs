4914b3cd011bd3e8e77c287c17c4f9c99337136f
Карина Слабодник <slabodnikkarina395@gmail.com>
2026-04-22 10:30:37 +0300

    Add files via upload

---
+++ Task4/Movie.Data.cs
@@ -0,0 +1,10 @@
+﻿namespace Task4
+{
+    public partial class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int Duration { get; set; } 
+        public string Genre { get; set; }
+    }
+}
---
+++ Task4/Movie.Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task4
+{
+    public partial class Movie
+    {
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Title} ({Genre}) - {Duration} мин, реж. {Director}");
+        }
+    }
+
+    public class Cinema
+    {
+        private Movie[] movies;
+
+        public Cinema(Movie[] movies)
+        {
+            this.movies = movies;
+        }
+
+        public Movie GetLongestMovie()
+        {
+            return movies.OrderByDescending(m => m.Duration).FirstOrDefault();
+        }
+
+        public Movie[] GetMoviesByDirector(string director)
+        {
+            return movies.Where(m => m.Director == director).ToArray();
+        }
+    }
+}
