b98d27e71851093c8191a1dae13d3fef753e9cc1
Карина Слабодник <slabodnikkarina395@gmail.com>
2026-04-23 10:05:01 +0300

    Update Program.cs

--- Day4/Task1/Program.cs
+++ Day4/Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello world from C# programming language";
+            int count = WordCount(text);
+            Console.WriteLine($"Строка: \"{text}\"");
+            Console.WriteLine($"Количество слов: {count}");
+        }
+
+        static int WordCount(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return 0;
+            return str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
